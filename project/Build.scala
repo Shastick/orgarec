0c16db93b6fed705753241d287caba7e2556af07
Julien Perrochet <julien.perrochet@epfl.ch>
2012-08-30 15:33:59 +0200

    draft project repartition.

--- project/Build.scala
+++ project/Build.scala
@@ -8,4 +8,46 @@ import Keys._
 import com.untyped.sbtrunmode.Plugin._
 
 object OrgaRecBuild extends Build {
+
+  scalaVersion := "2.9.0-1"
+
+  lazy val liftVersion ="2.4"
+
+  lazy val liftProjectSettings = webSettings ++ Seq(
+    libraryDependencies ++= Seq(
+      "org.mortbay.jetty" % "jetty" % "6.1.22" % "container",
+      "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
+      "ch.qos.logback" % "logback-classic" % "0.9.26"
+    )) ++ runModeSettings
+
+  lazy val storage_settings = Seq(
+  	libraryDependencies ++= Seq(
+	  "postgresql" % "postgresql" % "9.1-901.jdbc4"
+  ))
+
+  lazy val root = Project(
+  	id = "OrgaRec",
+	base = file(".")
+  ) aggregate(input,model,storage,view)
+
+  lazy val input = Project(
+   	id = "input",
+	base = file("core")
+  ) dependsOn(model)
+
+  lazy val model = Project(
+  	id = "model",
+	base = file("model")
+  ) dependsOn(storage)
+
+  lazy val storage = Project(
+  	id = "storage",
+	base = file("storage")
+  ) settings(storage_settings : _*) 
+
+  lazy val view = Project(
+  	id = "view",
+	base = file("view")
+  ) dependsOn(model)
 } 
+
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,7 +2,7 @@
 
 resolvers += Classpaths.typesafeResolver
 
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.0")
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.0-M2")
 
 // Web plugin
 
