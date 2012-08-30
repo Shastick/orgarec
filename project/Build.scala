21162bfda19344794d9b36d098e09cc4a7e33268
Julien Perrochet <julien.perrochet@epfl.ch>
2012-08-30 08:50:22 +0200

    Renaming...

--- project/Build.scala
+++ project/Build.scala
@@ -7,9 +7,5 @@ import AssemblyKeys._
 import Keys._
 import com.untyped.sbtrunmode.Plugin._
 
-object FlizitBuild extends Build {
-
-  scalaVersion := "2.9.0-1"
-
-  lazy val liftVersion = "2.4"
- } 
+object OrgaRecBuild extends Build {
+} 
