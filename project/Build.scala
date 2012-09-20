4815b4c044f43045abdd8e7f7a3f1a415e10c6da
Christopher Chiche <christopher.chiche@gmail.com>
2012-09-20 16:06:12 +0200

    added lift dependancies

--- project/Build.scala
+++ project/Build.scala
@@ -13,11 +13,19 @@ object OrgaRecBuild extends Build {
 
   lazy val liftVersion ="2.4"
 
-  lazy val liftProjectSettings = webSettings ++ Seq(
-    libraryDependencies ++= Seq(
-      "org.mortbay.jetty" % "jetty" % "6.1.22" % "container",
-      "net.liftweb" %% "lift-webkit" % liftVersion % "compile"
-    )) ++ runModeSettings
+  lazy val liftProjectSettings = Seq(libraryDependencies ++= Seq(
+        "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+        "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default",
+	"net.liftweb" %% "lift-wizard" % liftVersion % "compile->default", 
+	"org.eclipse.jetty" % "jetty-webapp" % "8.0.4.v20111024" % "container", // For Jetty 8
+	//"org.eclipse.jetty" % "jetty-webapp" % "7.3.0.v20110203" % "container", // For Jetty 7
+	//"org.mortbay.jetty" % "jetty" % "6.1.22" % "jetty,test", // For Jetty 6, add scope test to make jetty avl. for tests
+  	"org.scala-tools.testing" % "specs_2.9.0" % "1.6.8" % "test", // For specs.org tests
+  	"junit" % "junit" % "4.8" % "test->default", // For JUnit 4 testing
+  	"javax.servlet" % "servlet-api" % "2.5" % "provided->default",
+  	"com.h2database" % "h2" % "1.2.138", // In-process database, useful for development systems
+  	"ch.qos.logback" % "logback-classic" % "0.9.26" % "compile->default" // Logging
+     ))++ webSettings ++ runModeSettings
 
   lazy val storage_settings = Seq(
   	libraryDependencies ++= Seq(
