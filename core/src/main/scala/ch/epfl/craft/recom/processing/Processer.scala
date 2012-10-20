4d7dd59e8a46f83f88b1fb35daf94a75ee99145d
Julien Perrochet <julien.perrochet@epfl.ch>
2012-10-20 15:37:29 +0200

    saved various dump files and new Processer.scala

---
+++ core/src/main/scala/ch/epfl/craft/recom/processing/Processer.scala
@@ -0,0 +1,10 @@
+package ch.epfl.craft.recom.processing
+
+/** 
+ * The processer trait holds everything that 'will probably take some time' to be done and, generally,
+ * what is not computed/analyzed/processed at insert or view time.
+ */
+trait Processer {
+  def computeCoStudents
+
+}
