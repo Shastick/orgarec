7f888b23956a1c8a0d683c55a80e9c8f32929268
Julien Perrochet <julien.perrochet@epfl.ch>
2012-10-25 17:19:04 +0200

    removed unused view def

--- core/src/main/sqlview/coursecostudentcount.sql
+++
@@ -1,6 +0,0 @@
--- Show costudents quantity between any two courses.
-CREATE OR REPLACE VIEW coursecostudentscount AS 
- SELECT s1.course AS c1id, s2.course AS c2id, count(s1.student) AS count
-   FROM subscribed s1, subscribed s2
-  WHERE s1.course < s2.course AND s1.student = s2.student
-  GROUP BY s1.course, s2.course;
