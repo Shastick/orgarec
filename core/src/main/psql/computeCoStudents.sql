66470b026f20d27f8c16dc0cbd472b02120596da
Julien Perrochet <julien.perrochet@epfl.ch>
2012-10-29 14:06:24 +0100

    correction...

--- core/src/main/psql/computeCoStudents.sql
+++ core/src/main/psql/computeCoStudents.sql
@@ -1,16 +1,14 @@
 INSERT INTO courserelationmap("value", to_c, from_c, relation)
-SELECT count,c2id,c1id,'costudents'
-FROM coursecostudentscount;
-SELECT 
-	count(s1.student),
-	s2.course,
-	s1.course, 
-	'costudents'
+SELECT
+        count(s1.student),
+        s2.course,
+        s1.course,
+        'costudents'
    FROM subscribed s1,
-	subscribed s2
+        subscribed s2
 
   WHERE s1.course < s2.course
-	AND s1.student = s2.student
-	
+        AND s1.student = s2.student
+
   GROUP BY s1.course,
-	s2.course
+        s2.course
