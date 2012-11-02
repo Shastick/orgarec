d5aea041eb1bf0df27e9efaaf58e6ffbcf7f85a8
Julien Perrochet <julien.perrochet@epfl.ch>
2012-11-02 10:32:08 +0100

    stored proc for select section topics (faster than playing with mapper...)

---
+++ core/src/main/plpgsql/sectionTopics.sql
@@ -0,0 +1,33 @@
+-- return the topics teached by the specified sections
+-- usage: sectionTopics('{IN,SC}'::varchar[])
+CREATE OR REPLACE FUNCTION sectionTopics(
+	IN sid character varying[],
+	OUT t_name character varying,
+	OUT t_id character varying,
+	OUT s_name character varying
+)
+RETURNS SETOF record AS
+$$
+DECLARE
+	retval RECORD;
+BEGIN
+  FOR retval IN
+	SELECT
+		tm.name as tname, tm.id as tid, sm.name as sname
+	FROM
+		topicmap tm,
+		sectionmap sm
+	WHERE 
+		tm.section_c = sm.id
+		AND sm.name ilike any(sid)
+  LOOP
+	t_name:= retval.tname;
+	t_id:= retval.tid;
+	s_name:= retval.sname;
+	RETURN NEXT;
+  END LOOP;
+  RETURN;
+ END;
+ $$
+ LANGUAGE plpgsql;
+	
