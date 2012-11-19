6822ada7ebc2aba861eef65b3ac7783815605f7c
Julien Perrochet <julien.perrochet@epfl.ch>
2012-11-19 08:51:39 +0100

    trolilol forgotten file add ;-)

---
+++ view/src/main/scala/ch/epfl/craft/view/model/UserDisplay.scala
@@ -0,0 +1,22 @@
+package ch.epfl.craft.view.model
+import net.liftweb.http.SessionVar
+
+/**
+ * Session variable holding what nodes the user has on his screen.
+ */
+object UserDisplay extends SessionVar[(List[Node], List[Link])](List.empty, List.empty) {
+	
+	def nodes = is._1
+	def links = is._2
+	
+	def setLinks(ll: List[Link]) = performAtomicOperation(set(nodes, ll))
+	def setNodes(nl: List[Node]) = performAtomicOperation(set(nl,links))
+	
+	def addLinks(ll: List[Link]) = performAtomicOperation(set(nodes,links ++ ll))
+	def addNodes(nl: List[Node]) = performAtomicOperation(set(nodes ++ nl, links))
+	
+	def removeLinks(ll: List[Link]) = performAtomicOperation(set(nodes,links.diff(ll)))
+	def removeNodes(nl: List[Node]) = performAtomicOperation(set(nodes.diff(nl),links))
+	
+	def render = D3Graph(nodes,links).toJson
+}
